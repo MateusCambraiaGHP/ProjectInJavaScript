daf79f1ff52c80bf885fb7729e5954b390f02beb
Mateus.Cambraia <mateuscambraia.0246@aluno.una.br>
2022-05-30 17:24:29 -0300

    #3

---
+++ introducao-javascript/js/removerpaciente.js
@@ -0,0 +1,7 @@
+botaoEnviar.addEventListener("click", function (event) {
+
+    let form = document.querySelector("#formform");
+    let tabelaPacientes = document.querySelector("#tabela-pacientes");
+    
+    let paciente = obtemDadosPaciente(form);
+});
